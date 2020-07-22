2840670e327d3d866e8527f6a0e276546ab5b7cc
translines <margofss@yandex.ru>
2020-07-22 22:29:58 +0300

    adaptive first

--- js/main.js
+++ js/main.js
@@ -4,15 +4,29 @@ $(function () {
       '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
     nextArrow:
       '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
-      autoplay: true,
-      fade: true
+    autoplay: true,
+    fade: true,
   });
   $(".our-trip__slider").slick({
     prevArrow:
       '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
     nextArrow:
       '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
-      autoplay: true,
-      fade: true
+    autoplay: true,
+    fade: true,
+    responsive: [
+      {
+        breakpoint: 601,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
+  $(".menu, .our-trip__body").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr("href"),
+      top = $(id).offset().top;
+    $("body,html").animate({ scrollTop: top }, 1500);
   });
 });
